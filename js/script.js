fcf2a7d4f214834a84abca9962609fd9c8d6fa38
Iconsiststress20 <ST10491032@rcconnect.edu.za>
2025-11-21 13:15:54 +0200

    comit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 const searchIcon = document.querySelector('.search-icon')
 const searchForm = document.querySelector('.search-form')
+const menuIcon = document.querySelector('.menu-icon')
+const navbar = document.querySelector('.navbar')
+
+
 searchIcon.addEventListener('click', () =>{
 searchForm.classList.add('active')
+cartItemsContainer.classList.remove('active')
+
+})
+menuIcon.addEventListener('click', () =>{
+navbar.classList.add('active')
+searchForm.classList.remove('active')
 cartItemsContainer.classList.remove('active')
 
 })
@@ -11,4 +21,6 @@ const  cartItemsContainer = document.querySelector('.cart-items-container')
 cartIcon.addEventListener('click', () =>{
 cartItemsContainer.classList.add('active')
 searchForm.classList.remove('active')
+navbar.classList.remove('active')
+
 })
